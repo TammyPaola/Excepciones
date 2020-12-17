ea5879b946f6812e6fa6ce7a1b01968b74d8261b
TammyPaola <tpc.acosta@yavirac.edu.ec>
2020-12-17 18:46:54 -0500

    deber de ejemplo de excepcion

---
+++ EjemploDeber13122020 deber 3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeberEjemploApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continua;
+            do
+            {
+                try
+                {
+                    continua = false;
+                    Console.Write("Ingrese un valor:");
+                    string linea = Console.ReadLine();
+                    var num = int.Parse(linea);
+                    var cuadrado = num * num;
+                    Console.WriteLine($"El cuadrado de {num} es {cuadrado}");
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Debe ingresar obligatoriamente un número entero.");
+                    continua = true;
+                }
+            } while (continua);
+            Console.ReadKey();
+        }
+
+    }
+}
